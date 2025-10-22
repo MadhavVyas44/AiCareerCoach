7a3c531cb8519739cdfb2cd9ebeea781eea79e1e
MadhavVyas44 <madhav040404@gmail.com>
2025-10-22 22:40:13 +0530

    added a banner and grid background

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,9 +1,29 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
 const HeroSection = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
@@ -12,13 +32,13 @@ const HeroSection = () => {
             Your AI Career Coach for <br />
             Professional success
           </h1>
-          <p>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
             Advance your career with personalized guidance, interview prep and
             AI powered tools for job success.
           </p>
         </div>
 
-        <div>
+        <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
             <Button size="lg" className="px-8">
               Get Started
@@ -31,8 +51,8 @@ const HeroSection = () => {
           </Link>
         </div>
 
-        <div>
-          <div>
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
             <Image
               src={"/banner2.jpeg"}
               width={1280}
