63c9900cd109bc3487ce00a728d41cfc3f825593
MadhavVyas44 <madhav040404@gmail.com>
2025-10-29 06:52:33 +0530

    added an accordion with faqs

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,19 @@ import { Card, CardContent } from "@/components/ui/card";
 import { features } from "@/data/features";
 import { industries } from "./../data/industries";
 import { howItWorks } from "@/data/howItWorks";
+import { testimonial } from "@/data/testimonial";
+
+import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { faqs } from "@/data/faqs";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
@@ -90,6 +103,111 @@ export default function Home() {
                 </div>
               );
             })}
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-muted/50">
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-3xl text-center font-bold tracking-tighter mb-12">
+            What our Users say
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
+            {testimonial.map((testimonial, index) => {
+              return (
+                <Card
+                  key={index}
+                  className="bg-background border-2 hover:border-primary transition-colors duration-300"
+                >
+                  <CardContent className="pt-6 ">
+                    <div className="flex flex-col space-y-4">
+                      <div className="flex items-center space-x-4">
+                        <div className="relative h-12 w-12 flex-shrink-0">
+                          <Image
+                            width={40}
+                            height={40}
+                            src={testimonial.image}
+                            alt={testimonial.author}
+                            className="rounded-full object-cover border-2 border-primary/20"
+                          ></Image>
+                        </div>
+                        <div>
+                          <p className="font-semibold">{testimonial.author}</p>
+                          <p className="text-sm text-muted-foreground">
+                            {testimonial.role}
+                          </p>
+                          <p className="text-sm text-primary">
+                            {testimonial.company}
+                          </p>
+                        </div>
+                      </div>
+
+                      <blockquote>
+                        <p className="text-muted-foreground italic relative">
+                          <span
+                            className="text-2xl text-primary absolute -top-4 
+                          -left-2"
+                          >
+                            &quot;
+                          </span>
+                          {testimonial.quote}
+                          <span className="text-2xl text-primary absolute -bottom-1">
+                            &quot;
+                          </span>
+                        </p>
+                      </blockquote>
+                    </div>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-3xl text-center font-bold tracking-tighter mb-12">
+            Frequently asked question
+          </h2>
+          <p className="text-muted-foreground">
+            Find answers to common questions about our platform
+          </p>
+          <div className="max-w-6xl mx-auto">
+            <Accordion type="single" collapsible className="w-full">
+              {faqs.map((faq, index) => {
+                return (
+                  <AccordionItem key={index} value={`item-${index}`}>
+                    <AccordionTrigger>{faq.question}</AccordionTrigger>
+                    <AccordionContent>{faq.answer} </AccordionContent>
+                  </AccordionItem>
+                );
+              })}
+            </Accordion>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full">
+        <div className=" mx-auto py-24 gradient rounded-lg">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center max-w-3xl mx-auto">
+            <h2 className="text-3xl font-bold tracking-tighter text-primary-foreground sm:text-4xl md:text-5xl">
+              Ready to Accelerate Your Career?
+            </h2>
+            <p className="mx-auto text-primary-foreground/80 max-w-[600px] md:text-xl">
+              Join thousands of professionals who are advancing their careers
+              with AI-powered guidance{" "}
+            </p>
+            <Link href="/dashboard" passHref>
+              <Button
+                size="lg"
+                variant="secondary"
+                className=" h-11 mt-5 animate-bounce"
+              >
+                Start Your Journey Today!
+                <ArrowRight className="ml-2 h-4 w-4" />
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
