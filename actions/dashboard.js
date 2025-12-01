374531b0b01c916bb47ee4ab81537b3cf3fb6c12
MadhavVyas44 <madhav040404@gmail.com>
2025-12-01 16:13:18 +0530

    bug resolve

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -54,6 +54,10 @@ export async function getIndustryInsights() {
   });
   if (!user) throw new Error("User not found");
 
+  if (!user.industry) {
+    return null;
+  }
+
   if (!user.industryInsight) {
     const insights = await generateAIInsights(user.industry);
 
